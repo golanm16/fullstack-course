4f936dddb0ee8bb5a87ad52831e7a3cae25a5bbb
Golan <golanm16@gmail.com>
2021-10-24 20:31:47 +0300

    mis lesson 03

---
+++ lesson_03/lesson_03_notes.js
@@ -0,0 +1,13 @@
+/*
+notes:
+                                             
+*/
+
+let flag = true
+
+let result = flag ? 'a' : 'b'
+
+let str = 'hello'
+let name1 = 'dudu'
+
+console.log(`${str} ${name1}`)
