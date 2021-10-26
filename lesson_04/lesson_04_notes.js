8fd6f91a79a550e198e2527f808ed912265db07e
Golan <golanm16@gmail.com>
2021-10-26 20:33:19 +0300

    finished lesson 04

---
+++ lesson_04/lesson_04_notes.js
@@ -0,0 +1,34 @@
+// // // for loops
+// // let counter = 1;
+// // while(counter<=10){
+// //     console.log(counter);
+// //     counter++;
+// // }
+
+// // for(counter = 1;counter<=10;counter++){
+// //     console.log(counter);                         
+// // }
+// // console.log('fast log');
+
+// for (i = 1; i <= 100; i++) {
+//     console.log(i);
+// }
+
+// for (i = 1; i <= (120 / 4); i++) {
+//     console.log(i * 4);
+// }
+
+// let sum = 0;
+// for (i = 0; i < 5; i++) {
+//     let input = prompt("enter a number to sum");
+//     if (isNaN(input)) {
+//         console.log("Error: not a number. exiting...");
+//         break;
+//     }
+//     else {
+//         sum += Number(input);
+//     }
+// }
+// console.log('the sum is: ' + sum);
+
+//
