5f21c45470af45fddd2e76da51ddbd2f3bdfa4e1
Golan <golanm16@gmail.com>
2021-11-09 09:37:08 +0200

    created shortcut file

--- week_01/week_01_notes.js
+++ week_01/week_01_notes.js
@@ -1,7 +1,8 @@
 /*
-alt+shoft+up/down: duplicate line
+alt+shift+up/down: duplicate line
 alt+up/down: move currnet line
 ctrl+g: jump to line
-ctrl+p: current folder files
 ctrl+shift+f: search globally 
+ctrl+p: current folder files
 */
+
