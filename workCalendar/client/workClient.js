35d9cfb8d8bc5e36832e2f912168057e8d61ec47
Golan <golanm16@gmail.com>
2022-01-09 19:00:21 +0200

    workserver now connects to database

--- workCalendar/client/workClient.js
+++ workCalendar/client/workClient.js
@@ -1,138 +1,164 @@
-const url = "http://localhost:1521"
+import { PUNCH_IN, PUNCH_OUT, PORT, URL } from "../constants.js";
 
-const mainElem = document.getElementById('main');
+const url = `${URL}:${PORT}`;
+
+const mainElem = document.getElementById("main");
 
 function getAllWorkers() {
-    fetch(url + '/workers')
-        .then(res => res.json())
-        .then(workers => printList('list of all workers:', workers));
+  fetch(url + "/workers")
+    .then((res) => res.json())
+    .then((workers) => printList("list of all workers:", workers));
 }
 
 function getShifts() {
-    const id = document.getElementById('idInp').value || '';
-    fetch(url + `/shifts/${id}`)
-        .then(res => res.json())
-        .then(shifts => printList(id ? 'list of current worker shifts:' : 'all workers shifts:', shifts));
+  const id = document.getElementById("idInp").value || "";
+  fetch(url + `/shifts/${id}`)
+    .then((res) => res.json())
+    .then((shifts) =>
+      printList(
+        id ? "list of current worker shifts:" : "all workers shifts:",
+        shifts
+      )
+    );
 }
 
 function getHours() {
-    const id = document.getElementById('idInp').value || '';
-    console.log(id);
-    fetch(url + `/hours/${id}`)
-        .then(res => res.json())
-        .then(hours => {
-            printValue(id ? 'amount of current worker hours worked:' : 'sum of all workers hours worked:', hours)
-        });
+  const id = document.getElementById("idInp").value || "";
+  console.log(id);
+  fetch(url + `/hours/${id}`)
+    .then((res) => res.json())
+    .then((hours) => {
+      printValue(
+        id
+          ? "amount of current worker hours worked:"
+          : "sum of all workers hours worked:",
+        hours
+      );
+    });
 }
 
 function getSalary() {
-    const id = document.getElementById('idInp').value || '';
-    console.log(id);
-    fetch(url + `/salary/${id}`)
-        .then(res => res.json())
-        .then(hours => {
-            printValue(id ? 'current worker salary:' : 'sum of all workers salaries:', hours)
-        });
+  const id = document.getElementById("idInp").value || "";
+  console.log(id);
+  fetch(url + `/salary/${id}`)
+    .then((res) => res.json())
+    .then((hours) => {
+      printValue(
+        id ? "current worker salary:" : "sum of all workers salaries:",
+        hours
+      );
+    });
 }
-
-
 
 const getChosenTime = () => {
-    const time = document.getElementById('punchTimeInp');
-    const date = document.getElementById('punchDateInp');
-    let dateTimeNum = time.valueAsNumber + date.valueAsNumber;
-    if (!dateTimeNum) {
-        dateTimeNum = new Date().getTime();
-    }
-    return dateTimeNum;
-}
+  const time = document.getElementById("punchTimeInp");
+  const date = document.getElementById("punchDateInp");
+  let dateTimeNum = time.valueAsNumber + date.valueAsNumber;
+  if (!dateTimeNum) {
+    dateTimeNum = new Date().getTime();
+  }
+  return dateTimeNum;
+};
 
 function punchIn() {
-    const id = document.getElementById('idInp').value || -1;
-    const dateTimeNum = getChosenTime();
-    console.log(`${url}/punchtime/${id}/IN/${dateTimeNum}`);
-    fetch(`${url}/punchtime/${id}/IN/${dateTimeNum}`)
-        .then(res => res.json())
-        .then(worker => {
-            printWorker(worker);
-            if (worker.id) {
-                document.getElementById('idInp').value = worker.id;
-            }
-        });
+  const id = document.getElementById("idInp").value || -1;
+  const dateTimeNum = getChosenTime();
+  console.log(`fetching ${url}/punchtime/${id}/${PUNCH_IN}/${dateTimeNum}`);
+  fetch(`${url}/punchtime/${id}/${PUNCH_IN}/${dateTimeNum}`)
+    .then((res) => res.json())
+    .then((worker) => {
+      printWorker(worker);
+      if (worker.id) {
+        document.getElementById("idInp").value = worker.id;
+      }
+    });
 }
 
 function punchOut() {
-    const id = document.getElementById('idInp').value;
-    const dateTimeNum = getChosenTime();
-    fetch(`${url}/punchtime/${id}/OUT/${dateTimeNum}`)
-        .then(res => res.json())
-        .then(worker => printWorker(worker));
+  const id = document.getElementById("idInp").value;
+  const dateTimeNum = getChosenTime();
+  fetch(`${url}/punchtime/${id}/${PUNCH_OUT}/${dateTimeNum}`)
+    .then((res) => res.json())
+    .then((worker) => printWorker(worker));
 }
 
 const printWorker = (msg) => {
-    document.getElementById('worker').innerText = JSON.stringify(msg);
-}
+  document.getElementById("worker").innerText = JSON.stringify(msg);
+};
 
 const printList = (msg, list) => {
-    document.getElementById('list').innerText = msg + '\n' + JSON.stringify(list);
-}
+  document.getElementById("list").innerText = msg + "\n" + JSON.stringify(list);
+};
 
 const printValue = (msg, value) => {
-    document.getElementById('value').innerText = msg + '\n' + JSON.stringify(value);
-}
+  document.getElementById("value").innerText =
+    msg + "\n" + JSON.stringify(value);
+};
 
 const MainForm = () => {
-    return (
-        <React.Fragment>
-            <input type="number" id="idInp" placeholder='worker id' />
-            <button type="button" className="collapsible">change time. default is now</button>
-            <div className="content">
-                <input type="time" name="punchTime" id="punchTimeInp" />
-                <input type="date" min="2021-01-25" max="2021-12-29" name="punchDate" id="punchDateInp" />
-            </div>
-            <button id="inBtn" onClick={punchIn}>punch in</button>
-            <button id="outBtn" onClick={punchOut}>punch out</button>
-            <label>get list:</label>
-            <select>
-                <option onClick={getAllWorkers}>workers</option>
-                <option onClick={getShifts}>current worker shifts</option>
-                <option onClick={getHours}>current worker hours worked</option>
-                <option onClick={getSalary}>current worker salaries</option>
-                <option onClick={getShifts}>current worker shifts</option>
-            </select>
-            <div className="msgs">
-                <div id="worker"></div>
-                <div id="value"></div>
-                <div id="list"></div>
-            </div>
-        </React.Fragment>
-    )
-}
+  return (
+    <React.Fragment>
+      <input type="number" id="idInp" placeholder="worker id" />
+      <button type="button" className="collapsible">
+        change time. default is now
+      </button>
+      <div className="content">
+        <input type="time" name="punchTime" id="punchTimeInp" />
+        <input
+          type="date"
+          min="2021-01-25"
+          max="2021-12-29"
+          name="punchDate"
+          id="punchDateInp"
+        />
+      </div>
+      <button id="inBtn" onClick={punchIn}>
+        punch in
+      </button>
+      <button id="outBtn" onClick={punchOut}>
+        punch out
+      </button>
+      <label>get list:</label>
+      <select>
+        <option onClick={getAllWorkers}>workers</option>
+        <option onClick={getShifts}>current worker shifts</option>
+        <option onClick={getHours}>current worker hours worked</option>
+        <option onClick={getSalary}>current worker salaries</option>
+        <option onClick={getShifts}>current worker shifts</option>
+      </select>
+      <div className="msgs">
+        <div id="worker"></div>
+        <div id="value"></div>
+        <div id="list"></div>
+      </div>
+    </React.Fragment>
+  );
+};
 
 const populateForm = async () => {
-    await ReactDOM.render(<MainForm />, mainElem);
-}
+  await ReactDOM.render(<MainForm />, mainElem);
+};
 
 function makeCollapsible() {
-    let coll = document.getElementsByClassName("collapsible");
-    let i;
+  let coll = document.getElementsByClassName("collapsible");
+  let i;
 
-    for (i = 0; i < coll.length; i++) {
-        coll[i].addEventListener("click", function () {
-            this.classList.toggle("active");
-            let content = this.nextElementSibling;
-            if (content.style.display === "flex") {
-                content.style.display = "none";
-            } else {
-                content.style.display = "flex";
-            }
-        });
-    }
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+      this.classList.toggle("active");
+      let content = this.nextElementSibling;
+      if (content.style.display === "flex") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "flex";
+      }
+    });
+  }
 }
 
 async function main() {
-    await populateForm();
-    makeCollapsible();
+  await populateForm();
+  makeCollapsible();
 }
 
 main();
--- workCalendar/mongodb/index.js
+++ workCalendar/mongodb/index.js
@@ -1,28 +1,57 @@
-const key = require("./secret").MongoKey;
-const user = require("./secret").MongoUser;
-const mongoUrl = `mongodb+srv://${user}:${key}@cluster0.eeeyx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+import { MongoKey, MongoUser } from "./secret.js";
+const mongoUrl = `mongodb+srv://${MongoUser}:${MongoKey}@cluster0.eeeyx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 console.log(mongoUrl);
 
-async function conn() {
-    try {
-        const connection = await mongoose.connect(
-            mongoUrl
-        )
-        const Cat = connection.model("cat", { catName: String });
-        const kitty = new Cat({ catName: "zelda the 3rd" });
-        kitty
-            .save()
-            .then(() => connection.disconnect())
-            .then(console.log("MMMM"));
-    }
-    catch (e) {
-        console.log(e);
-    }
+const getConnection = async () => {
+  try {
+    return await mongoose.connect(mongoUrl);
+  } catch (e) {
+    console.log(e);
+  }
+};
+const Cat = mongoose.model("cat", { catName: String });
 
+async function saveCat() {
+  try {
+    const connection = await getConnection();
+    const cat = { catName: "cat from outer model" };
+    const kitty = new Cat(cat);
+    const some = await kitty.save();
+    console.log(some);
+    const dis = await connection.disconnect();
+    console.log(dis);
+  } catch (e) {
+    console.log(e);
+  }
 }
 
-conn();
+async function find() {
+  try {
+    const connection = await getConnection();
+    const cat = await Cat.find({ catName: "zelda the 4th" });
+    console.log(cat);
+    const dis = await connection.disconnect();
+    console.log(dis);
+  } catch (e) {
+    console.log(e);
+  }
+}
+async function findOneAndUpdate() {
+  try {
+    const connection = await getConnection();
+    const beforeUpdate = await Cat.findOneAndUpdate(
+      { catName: "zelda" },
+      { catName: "zelda the 1st" }
+    );
+    console.log(beforeUpdate);
+    const dis = await connection.disconnect();
+    console.log(dis);
+  } catch (e) {
+    console.log(e);
+  }
+}
 
+saveCat();
